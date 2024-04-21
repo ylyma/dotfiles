e8f08d6beec667ee633ee85abcd171ef5f3093ae
ylyma <amylingyuhui@gmail.com>
2024-04-21 23:47:47 +0800

    update ranger

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 alias todo='task ready'
 alias dtime='timew day'
 alias spot='ncspot'
+alias panmd2pdf='pandoc --from=markdown --to=pdf -V fonsize=12t -V colorlinks -V indent=true -V documentclass=amsart -V linestretch=1.5'
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end

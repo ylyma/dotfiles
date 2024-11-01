acb5487a4e6abc68c490a2f4b016a7e3c9b02c92
ylyma <amylingyuhui@gmail.com>
2024-11-01 20:26:50 +0800

    add nnn

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,16 @@
+export EDITOR="nvim"
+
 alias todo='task ready'
-alias dtime='timew day'
-alias wtime='timew week'
-alias wsumtime='timew summary :week'
-alias dsumtime='timew summary'
 alias panmd2pdf='pandoc --from=markdown --to=pdf -V fonsize=12t -V colorlinks -V indent=true -V documentclass=amsart -V linestretch=1.5'
 alias icat="kitten icat"
 alias s="kitten ssh"
 alias d="kitten diff"
 
-alias vim="nvim"
+alias vim='nvim'
 
 alias cln="latexmk -c"
 alias forti="sudo openfortivpn webvpn.comp.nus.edu.sg --username=e0969284"
+
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'

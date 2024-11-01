56367ef64f1d6d14d7eb847c8d00c394d060f602
ylyma <amylingyuhui@gmail.com>
2024-11-01 20:26:50 +0800

    add nnn

---
+++ .config/fish/conf.d/nnn.fish
@@ -0,0 +1,44 @@
+export NNN_PLUG='j:autojump;o:fzopen;p:preview-tui;d:diffs;t:nmount'
+export NNN_PLUG_COMMANDS='g:-!git diff;l:-!git log;x:!chmod +x "$nnn"'
+export NNN_FCOLORS="0B0B04060006060009060B06"
+alias ls="nnn -de"
+
+set --export NNN_FIFO "/tmp/nnn.fifo"
+
+
+# Rename this file to match the name of the function
+# e.g. ~/.config/fish/functions/n.fish
+# or, add the lines to the 'config.fish' file.
+
+function n --wraps nnn --description 'support nnn quit and change directory'
+    # Block nesting of nnn in subshells
+    if test -n "$NNNLVL" -a "$NNNLVL" -ge 1
+        echo "nnn is already running"
+        return
+    end
+
+    # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
+    # see. To cd on quit only on ^G, remove the "-x" from both lines below,
+    # without changing the paths.
+    if test -n "$XDG_CONFIG_HOME"
+        set -x NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
+    else
+        set -x NNN_TMPFILE "$HOME/.config/nnn/.lastd"
+    end
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    # The command function allows one to alias this function to `nnn` without
+    # making an infinitely recursive alias
+    command nnn $argv
+
+    if test -e $NNN_TMPFILE
+        source $NNN_TMPFILE
+        rm -- $NNN_TMPFILE
+    end
+end

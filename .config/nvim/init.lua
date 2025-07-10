e120dda90bc71a05407c5edf922fa8aed616b332
ylyma <amylingyuhui@gmail.com>
2025-07-10 10:59:04 +0800

    remove lsp servers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -692,15 +692,12 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        pyright = {},
-        taplo = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
         --
 
         lua_ls = {
